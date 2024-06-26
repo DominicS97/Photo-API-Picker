04e57712f48eade98e807223fc586d127819601a
DominicS97 <dejfsutcliffe@gmail.com>
2024-06-26 10:01:40 +0100

    Base html css and js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Photo-API-Picker
+
+<b>Task:</b> Use a public image API to create custom collections and associate them with a user's email
+
+<b>Tech:</b> HTML, CSS and JavaScript
---
+++ script.js
@@ -0,0 +1,6 @@
+const IMG = document.getElementById("target");
+let source = IMG.src;
+
+const COLLECTION = document.getElementById("collection");
+const DEST = document.getElementById("destination");
+DEST.src = source;

371bc50854513462faf8f22d50dbf4028cfd7883
DominicS97 <dejfsutcliffe@gmail.com>
2024-06-26 12:05:18 +0100

    new image and add image functions

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 // Image manipulation
 
 const IMG = document.getElementById("target");
-let source = IMG.src;
-
 const COLLECTION = document.getElementById("collection");
-const DEST = document.getElementById("destination");
-DEST.src = source;
 
+// Request new image
+
+function newImage() {
+	IMG.src = IMG.src + "?random&t=" + new Date().getTime();
+}
+
+// Add image to collection
+
+function repImage() {
+	COLLECTION.innerHTML += `<img src="${IMG.src}" />`;
+}
 // Email validation
 
-function changeEmail(email) {
-	const FORM = document.getElementById("form");
-	FORM.style.display = "none";
-	const EMAIL_DISPLAY = document.getElementById("email-display");
-	EMAIL_DISPLAY.style.display = "block";
-}
+const FORM = document.getElementById("form");
+const EMAIL_DISPLAY = document.getElementById("email-display");
 
 function validateEmail() {
 	const email = document.getElementById("email");
@@ -23,14 +26,19 @@ function validateEmail() {
 			/^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
 		let s = email.value;
 		if (regex.test(s)) {
-			const FORM = document.getElementById("form");
 			FORM.style.display = "none";
-			const EMAIL_DISPLAY = document.getElementById("email-display");
 			EMAIL_DISPLAY.style.display = "block";
 			const CUR_EMAIL = document.getElementById("current-email");
 			CUR_EMAIL.innerHTML = `${s}`;
 		} else {
 			alert("Email is not valid");
+			email.value = "";
+			s = "";
 		}
 	});
+}
+
+function newEmail() {
+	FORM.style.display = "block";
+	EMAIL_DISPLAY.style.display = "none";
 }
